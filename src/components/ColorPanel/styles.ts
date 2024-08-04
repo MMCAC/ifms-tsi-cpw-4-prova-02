3b4b13cb798e08aaad96f58b5b5dcd7bca6c9426
Maicon Cezar <43833424+MMCAC@users.noreply.github.com>
2024-08-03 21:38:19 -0400

    add dos files

---
+++ src/components/ColorPanel/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Panel = styled.div`
+    width: 300px;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
+    color: white;
+    text-align: center;
+`
